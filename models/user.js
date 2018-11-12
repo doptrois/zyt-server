04e15002a9448361cfc1774b9bd46a8d6760d72f
Michael Fischer <doptrois@gmail.com>
2018-11-13 00:13:05 +0100

    fixed missing objectId validation for Joi

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
