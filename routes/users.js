0a0c50b3b6599b9ae77d341423a207fab477bb76
Michael Fischer <doptrois@gmail.com>
2018-11-18 19:38:20 +0100

    cleanup, implemented user registration with hashed pw

--- routes/users.js
+++ routes/users.js
@@ -15,10 +15,13 @@ const populateConfig = [
         select: 'url'
     }
 ];
+
+const dbSelectProperties = 'isAdmin first_name surname archived avatar';
+
 router.get('/', async (req, res) => {
     const users = await User
         .find()
-        .select('first_name surname archived avatar')
+        .select(dbSelectProperties)
         .populate(populateConfig);
     res.send(users);
 });
@@ -26,7 +29,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', oIdValidator, async (req, res) => {
     const user = await User
         .findById(req.params.id)
-        .select('first_name surname archived avatar')
+        .select(dbSelectProperties)
         .populate(populateConfig);
     if (!user) return res.status(404).send('The user with the given ID was not found.');
     res.send(user);
@@ -35,11 +38,18 @@ router.get('/:id', oIdValidator, async (req, res) => {
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-    let user = new User(req.body);
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User alread registered');
+    user = new User(_.pick(req.body, ['first_name', 'surname', 'email', 'avatar', 'password']));
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
     user = await user.save();
-    res.send(user);
+    res.send(_.pick(req.body, ['_id', 'first_name', 'surname', 'email', 'avatar']));
 });
 
+// tbd
 router.put('/:id', oIdValidator, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -49,7 +59,7 @@ router.put('/:id', oIdValidator, async (req, res) => {
 });
 
 router.delete('/:id', oIdValidator, async (req, res) => {
-    const user = await User.findByIdAndUpdate(req.params.id, { archived: true }, { new: true });
+    let user = await User.findByIdAndUpdate(req.params.id, { archived: true });
     if (!user) return res.status(404).send('The user with the given ID was not found.');
     res.send(user);
 });
