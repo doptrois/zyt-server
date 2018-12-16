6f2c9a6e154d43b8bbfefcdf5bdb148f1554b773
Michael Fischer <doptrois@gmail.com>
2018-12-16 13:48:26 +0100

    added user id in localstorage data for user routes in case the user wants to modify his own data, it's easier to have it in plaintext in the localstorage.

--- routes/auth.js
+++ routes/auth.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
     if (!validPassword) return res.status(400).send('Invalid email or password.');
 
     const token = user.generateAuthToken();
-    res.send({token});
+    res.send({_id: user._id, token});
 });
 
 function validate(req) {
