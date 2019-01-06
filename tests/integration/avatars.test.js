6b97302ad16904c6af4cb97b01805a08c7cdfedd
doptrois <doptrois@gmail.com>
2019-01-06 20:39:39 +0100

    enhanced avatar test to be more specific.

--- tests/integration/avatars.test.js
+++ tests/integration/avatars.test.js
@@ -1,19 +1,24 @@
 const request = require('supertest');
 const { User } = require('../../models/user');
-const server = require('../../index');
+const { Avatar } = require('../../models/avatar');
+let server;
 
 describe('/api/avatars', () => {
     let token;
     beforeEach(() => {
+        server = require('../../index');
         token = new User().generateAuthToken();
     });
     afterEach(() => { server.close(); });
     describe('GET /', () => {
         it('should return all avatars', async () => {
+            const avatars = await Avatar
+                .find()
+                .sort('name');
             const res = await request(server)
                 .get('/api/avatars')
                 .set('x-auth-token', token);
-            expect(res.status).toBe(200);
+            expect(JSON.stringify(res.body)).toEqual(JSON.stringify(avatars));
         });
     });
 });
